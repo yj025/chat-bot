868f672304fc606bb3cc600a491ed62f33856d67
zouzheng <zheng.zou@thoughtworks.com>
2023-03-17 13:05:35 +0800

    add mic recorder

--- src/chat/InputBox.tsx
+++ src/chat/InputBox.tsx
@@ -1,5 +1,6 @@
 import { FC, useRef } from 'react'
 import style from './InputBox.module.css'
+import { useRecorder } from './useRecorder'
 
 interface Props {
   onSubmit: (content: string) => void
@@ -7,10 +8,17 @@ interface Props {
 
 export const InputBox: FC<Props> = ({ onSubmit }) => {
   const inputRef = useRef<HTMLInputElement>(null)
+  const [recording, startRecord, stopRecord] = useRecorder(onSubmit)
 
   return (
     <div className={style.inputBoxContainer}>
       <input ref={inputRef} className={style.input} />
+      <button
+        className={style.mic}
+        onClick={recording ? stopRecord : startRecord}
+      >
+        mic
+      </button>
       <button
         onClick={() => {
           const current = inputRef.current
---
+++ src/chat/useRecorder.ts
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+export const useRecorder = (onSuccess: (result: string) => void) => {
+  const [recording, setRecording] = useState(false)
+  const [recorder, setRecorder] = useState<MediaRecorder>()
+  const startRecord = () => {
+    let chunks: Blob[] = []
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      // 'then()' method returns a Promise
+      .then((mediaStreamObj) => {
+        setRecording(true)
+        console.log('new mediaStreamObj', mediaStreamObj)
+
+        const mediaRecorder = new MediaRecorder(mediaStreamObj)
+
+        mediaRecorder.ondataavailable = function (ev) {
+          console.log(chunks)
+          chunks.push(ev.data)
+        }
+
+        mediaRecorder.onstop = () => {
+          console.log('onStop')
+          const audioData = new Blob(chunks, { type: 'audio/mp3;' })
+          console.log(audioData)
+          const audioSrc = window.URL.createObjectURL(audioData)
+          onSuccess(audioSrc)
+
+          setRecording(false)
+          setRecorder(undefined)
+          chunks = []
+        }
+        setRecorder(mediaRecorder)
+        mediaRecorder.start()
+      })
+  }
+  const stopRecord = () => {
+    recorder?.stop()
+  }
+
+  return [recording, startRecord, stopRecord] as const
+}
