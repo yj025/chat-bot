696ca14677a1ac65a80baa8368075188748c4a73
yinjun <jun.yin@thoughtworks.com>
2023-06-01 18:28:43 +0800

    DTP-000[yinjun]chore: fix the eslint error in ChatBubble test

--- src/chat/__tests__/ChatBubble.spec.tsx
+++ src/chat/__tests__/ChatBubble.spec.tsx
@@ -1,10 +1,11 @@
 import {render, screen} from "@testing-library/react";
 import {ChatBubble} from "../ChatBubble";
 import {ChatSource} from "../ChatModel";
+import { likeChat } from "../../backend-proxy/api";
 
 describe('test Bubble', function () {
     it('should render Bubble', function () {
-        render(<ChatBubble chat={{from: ChatSource.SENDER, message: "biu biu"}} mark={jest.mock}/>)
+        render(<ChatBubble chat={{from: ChatSource.SENDER, message: "biu biu"}} likeChat={likeChat}/>)
         expect(screen.getByText("biu biu")).toBeInTheDocument()
     });
 });
